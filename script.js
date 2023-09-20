a54f745e548408e8946a20c9c47bc1261c95d900
Kenny Morales <kennymorales580@gmail.com>
2023-09-20 13:59:18 -0500

    Added custom icons for weather

--- script.js
+++ script.js
@@ -26,27 +26,50 @@ function fetchWeatherData() {
     });
 }
 
+// Mapping of OpenWeatherMap icon codes to your SVG icons
+const iconMap = {
+    '01d': 'SVG/clear-sky-day.svg',          // clear sky (day)
+    '01n': 'SVG/clear-sky-night.svg',    // clear sky (night)
+    '02d': 'SVG/few-clouds-day.svg',         // few clouds (day)
+    '02n': 'SVG/few-clouds-night.svg',   // few clouds (night)
+    '03d': 'SVG/scattered-clouds.svg',   // scattered clouds (day)
+    '03n': 'SVG/scattered-clouds.svg', // scattered clouds (night)
+    '04d': 'SVG/scattered-clouds.svg',      // broken clouds (day)
+    '04n': 'SVG/scattered-clouds.svg',// broken clouds (night)
+    '09d': 'SVG/shower-rain.svg',        // shower rain (day)
+    '09n': 'SVG/shower-rain.svg',  // shower rain (night)
+    '10d': 'SVG/rain-day.svg',               // rain (day)
+    '10n': 'SVG/rain-night.svg',         // rain (night)
+    '11d': 'SVG/thunderstorm.svg',       // thunderstorm (day)
+    '11n': 'SVG/thunderstorm.svg', // thunderstorm (night)
+    '13d': 'SVG/snow-day.svg',               // snow (day)
+    '13n': 'SVG/snow-night.svg',         // snow (night)
+    '50d': 'SVG/shower-rain.svg',               // mist (day)
+    '50n': 'SVG/shower-rain.svg'          // mist (night)
+};
+
 // Function to fetch weather data and display it
 async function showWeather() {
     navigator.geolocation.getCurrentPosition(async (position) => {
         const { latitude, longitude } = position.coords;
-
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+        
         try {
             const response = await fetch(apiUrl);
             const data = await response.json();
+            
             const cityName = data.name.toUpperCase();
             const countryName = data.sys.country.toUpperCase();
-            const temperatureCelsius = data.main.temp; // Temperature in Celsius
-            const temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius); // Convert to Fahrenheit
+            const temperatureCelsius = data.main.temp;
+            const temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
             const locationText = `${cityName}, ${countryName}`;
-            const temperatureText = `${temperatureFahrenheit.toFixed(2)}°F`; // Display temperature in Fahrenheit with 2 decimal places
+            const temperatureText = `${temperatureFahrenheit.toFixed(2)}°F`;
             const weatherText = data.weather[0].description.toUpperCase();
+            
+            // Determine which SVG icon to display
             const weatherIconCode = data.weather[0].icon;
-            const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}.png`;
-            document.getElementById('weather-icon').src = weatherIconUrl;
-
-
+            const weatherIconUrl = iconMap[weatherIconCode] || 'icons/default.svg'; // Use a default icon if the icon code is not in the map
+            
             // Display location, temperature, and weather data
             locationElement.textContent = locationText;
             temperatureElement.textContent = temperatureText;
